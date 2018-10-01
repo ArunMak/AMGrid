7f19ef5093ce95e969800e146cf2c032d9dcdf74
ArunMak <arunmakk@gmail.com>
2018-10-02 01:40:16 +0530

    Initial Commit

---
+++ AMGrid/AMGrid.h
@@ -0,0 +1,19 @@
+//
+//  AMGrid.h
+//  AMGrid
+//
+//  Created by ArunMak on 02/10/18.
+//  Copyright © 2018 ArunMak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AMGrid.
+FOUNDATION_EXPORT double AMGridVersionNumber;
+
+//! Project version string for AMGrid.
+FOUNDATION_EXPORT const unsigned char AMGridVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AMGrid/PublicHeader.h>
+
+
